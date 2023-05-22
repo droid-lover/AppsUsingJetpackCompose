501116dab856babf937f3b401c801f458c1a2898
droid-lover <ersachin20@gmail.com>
2023-05-22 18:34:24 +0530

    Implemented custom login ui events

---
+++ app/src/main/java/com/nativemobilebits/loginflow/data/LoginUIEvent.kt
@@ -0,0 +1,9 @@
+package com.nativemobilebits.loginflow.data
+
+sealed class LoginUIEvent{
+
+    data class EmailChanged(val email:String): LoginUIEvent()
+    data class PasswordChanged(val password: String) : LoginUIEvent()
+
+    object LoginButtonClicked : LoginUIEvent()
+}
