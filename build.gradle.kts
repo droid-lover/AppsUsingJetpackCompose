a93dea1cea0f992408435d40ba318c73cd59fa6b
droid-lover <ersachin20@gmail.com>
2023-05-22 18:35:33 +0530

    Added com.google.gms.google-services

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") version "7.4.0" apply false
     id("com.android.library") version "7.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.google.gms.google-services")version "4.3.15" apply false
 }
 
 tasks {
