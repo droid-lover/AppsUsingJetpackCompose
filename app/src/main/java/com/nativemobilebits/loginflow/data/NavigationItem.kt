528c1d06c74d1fab294b01c920827c4fc1b0d0fc
droid-lover <ersachin20@gmail.com>
2023-05-27 12:12:58 +0530

    A pojo class added for Navigation item

---
+++ app/src/main/java/com/nativemobilebits/loginflow/data/NavigationItem.kt
@@ -0,0 +1,10 @@
+package com.nativemobilebits.loginflow.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String,
+    val description: String,
+    val itemId: String,
+    val icon: ImageVector
+)
