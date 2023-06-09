7f93ba7ea55c8ae1f716e1ada20119588f48eb10
droid-lover <ersachin20@gmail.com>
2023-06-09 22:21:14 +0530

    Added homeviewmodel and using references to mutable live data

--- app/src/main/java/com/nativemobilebits/loginflow/app/PostOfficeApp.kt
+++ app/src/main/java/com/nativemobilebits/loginflow/app/PostOfficeApp.kt
@@ -1,11 +1,14 @@
 package com.nativemobilebits.loginflow.app
 
+import androidx.activity.viewModels
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.nativemobilebits.loginflow.data.home.HomeViewModel
 import com.nativemobilebits.loginflow.navigation.PostOfficeAppRouter
 import com.nativemobilebits.loginflow.navigation.Screen
 import com.nativemobilebits.loginflow.screens.HomeScreen
@@ -14,24 +17,34 @@ import com.nativemobilebits.loginflow.screens.SignUpScreen
 import com.nativemobilebits.loginflow.screens.TermsAndConditionsScreen
 
 @Composable
-fun PostOfficeApp() {
+fun PostOfficeApp(homeViewModel: HomeViewModel = viewModel()) {
+
+    homeViewModel.checkForActiveSession()
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
 
-        Crossfade(targetState = PostOfficeAppRouter.currentScreen) { currentState->
-            when(currentState.value){
-                is Screen.SignUpScreen ->{
+        if (homeViewModel.isUserLoggedIn.value == true) {
+            PostOfficeAppRouter.navigateTo(Screen.HomeScreen)
+        }
+
+        Crossfade(targetState = PostOfficeAppRouter.currentScreen) { currentState ->
+            when (currentState.value) {
+                is Screen.SignUpScreen -> {
                     SignUpScreen()
                 }
+
                 is Screen.TermsAndConditionsScreen -> {
                     TermsAndConditionsScreen()
                 }
-                is Screen.LoginScreen ->{
+
+                is Screen.LoginScreen -> {
                     LoginScreen()
                 }
-                is Screen.HomeScreen ->{
+
+                is Screen.HomeScreen -> {
                     HomeScreen()
                 }
             }
